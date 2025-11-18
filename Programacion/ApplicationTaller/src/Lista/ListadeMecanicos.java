731aca1053c3448531e716031af2a46d0cc99fb7
Jhon fonseca <jhonkennethfonseca99@gmail.com>
2025-11-18 15:48:09 -0600

    informcion necesaria del proyecto

---
+++ Programacion/ApplicationTaller/src/Lista/ListadeMecanicos.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Lista;
+
+/**
+ *
+ * @author Jhon fonseca
+ */
+public class ListadeMecanicos {
+   String Mecanicos;
+   
+    public ListadeMecanicos(){
+    }
+
+    public ListadeMecanicos(String Mecanicos) {
+        this.Mecanicos = Mecanicos;
+    }
+
+    public String getMecanicos() {
+        return Mecanicos;
+    }
+
+    public void setMecanicos(String Mecanicos) {
+        this.Mecanicos = Mecanicos;
+    }
+
+    
+}
